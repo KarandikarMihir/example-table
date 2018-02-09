7347c7f90795720d40e34a260c1da4b1e0591d99
Mihir Karandikar <karandikar.mihir@outlook.com>
2018-02-09 17:47:07 +0530

    Update docs

--- site/src/App.js
+++ site/src/App.js
@@ -32,6 +32,33 @@ class App extends Component {
         <p className="App-intro">
           See the table in action!
         </p>
+        <div className="docs">
+          <h2>Usage</h2>
+          <hr />
+          <pre dangerouslySetInnerHTML={{ __html: `
+            const columns = ['No.', 'Name', 'Email']
+            const rows = [
+              {
+                id: 1,
+                name: 'John',
+                email: 'john@gmail.com'
+              },
+              {
+                id: 2,
+                name: 'Joe',
+                email: 'joe@gmail.com'
+              },
+              {
+                id: 3,
+                name: 'Jane',
+                email: 'jane@gmail.com'
+              }
+            ]
+          ` }} />
+          <pre dangerouslySetInnerHTML={{ __html: `
+            &lt;SuperTable className="my-class" columns={columns} rows={rows} /&gt;
+          ` }} />
+        </div>
         <SuperTable columns={columns} rows={rows} />
       </div>
     );
